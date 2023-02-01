36c0d9f13aaab480956eddfab6348a9d6d9ad3d0
TrinhNT <nguyen.the.trinh@linecorp.com>
2023-02-01 22:23:21 +0700

    Create project and add .gitignore

---
+++ Noljanolja/NoljanoljaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoljanoljaApp.swift
+//  Noljanolja
+//
+//  Created by Nguyen The Trinh on 01/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NoljanoljaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
