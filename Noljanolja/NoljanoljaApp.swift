d65f2261fbe44df0f1c39b185ed6d562a91d4fd6
TrinhNT <nguyen.the.trinh@linecorp.com>
2023-02-01 22:24:33 +0700

    Config deployment target

--- Noljanolja/NoljanoljaApp.swift
+++ Noljanolja/NoljanoljaApp.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 @main
+struct MainApp {
+    static func main() {
+        if #available(iOS 14.0, *) {
+            NoljanoljaApp.main()
+        } else {
+            UIApplicationMain(
+                CommandLine.argc,
+                CommandLine.unsafeArgv,
+                nil,
+                NSStringFromClass(AppDelegate.self)
+            )
+        }
+    }
+}
+
+@available(iOS 14.0, *)
 struct NoljanoljaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+//@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {}
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
         }
     }
 }
