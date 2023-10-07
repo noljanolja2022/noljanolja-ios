648ec31a4d3257948877b4ce2079085d2ef3d613
TrinhNT <trinhnt.dev@gmail.com>
2023-10-08 00:27:49 +0700

    Update scheme and config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -53,7 +53,7 @@ kotlin {
         }
 
         // Maps custom Xcode configuration to NativeBuildType
-        xcodeConfigurationToNativeBuildType["Dev"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
-        xcodeConfigurationToNativeBuildType["AppStore"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
+        xcodeConfigurationToNativeBuildType["Debug"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
+        xcodeConfigurationToNativeBuildType["Release"] = org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.RELEASE
     }
 }
