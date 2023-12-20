bb6688e50aa32474476d2a3c5ee1748681e3f3c8
duydinhv <duydinh11198@gmail.com>
2023-12-20 13:44:06 +0700

    Implement Earned Points Popup

---
+++ Noljanolja/Sources/Main/Presentation/Views/EarnedPointsPopup/EarnedPointsPopupView.swift
@@ -0,0 +1,49 @@
+//
+//  EarnedPointsPopupView.swift
+//  Noljanolja
+//
+//  Created by duydinhv on 20/12/2023.
+//
+
+import SwiftUI
+
+// MARK: - EarnedPointsPopupView
+
+struct EarnedPointsPopupView: View {
+    let point: Int = 100
+    
+    var body: some View {
+        BottomSheet {
+            VStack {
+                ImageAssets.icEarnedPoints.swiftUIImage
+                    .scaledToFit()
+                    .height(38)
+                    .padding(.bottom, 10)
+
+                VStack(spacing: 12) {
+                    Text(L10n.transactionHistoryPoint(point))
+                        .dynamicFont(.systemFont(ofSize: 22, weight: .medium))
+                    Text(L10n.referralReceivePoint(point))
+                        .multilineTextAlignment(.center)
+                        .dynamicFont(.systemFont(ofSize: 14))
+                        .padding(.horizontal, 30)
+                }
+                .foregroundColor(ColorAssets.neutralDarkGrey.swiftUIColor)
+                .padding(.bottom, 50)
+
+                Button(L10n.commonOk.uppercased()) {}
+                    .buttonStyle(PrimaryButtonStyle())
+                    .padding(.horizontal, 10)
+            }
+            .padding(.top, 42)
+        }
+    }
+}
+
+// MARK: - EarnedPointsPopupView_Previews
+
+struct EarnedPointsPopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        EarnedPointsPopupView()
+    }
+}
